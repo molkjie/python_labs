46ab572926c3faca8fb1dbe7f0587a64c94ad198
Kate Chebanenko <ekaterinacebanenko2005@gmail.com>
2025-10-20 22:59:42 +0300

    Lab3: added templates, views, urls and static files for shop

--- lab2/shop/urls.py
+++ lab2/shop/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.shop_index, name='shop_index'),  # головна сторінка магазину
+    path('categories/', views.category_list, name='category_list'),
+    path('products/', views.product_list, name='product_list'),
 ]
--- lab2/shop/views.py
+++ lab2/shop/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
 
 def index(request):
     products = Product.objects.all()
     return render(request, 'shop/index.html', {'products': products})
+def shop_index(request):
+    # можна показати коротку сторінку або останні товари
+    products = Product.objects.all()[:5]
+    return render(request, 'shop/product_list.html', {'products': products})
+
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'shop/category_list.html', {'categories': categories})
+
+def product_list(request):
+    products = Product.objects.select_related('category').all()
+    return render(request, 'shop/product_list.html', {'products': products})
