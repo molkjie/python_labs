ea850136b832ff7ea52e5a1383b47e42de73f0ae
Kate Chebanenko <ekaterinacebanenko2005@gmail.com>
2025-12-16 20:57:58 +0200

    Lab10: Railway deploy setup, admin URL change

--- lab2/myproject/settings.py
+++ lab2/myproject/settings.py
@@ -149,3 +149,28 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
 }
+
+
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+import dj_database_url
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / ".env")
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = os.getenv("DEBUG", "False") == "True"
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
--- lab2/myproject/urls.py
+++ lab2/myproject/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+     path('mysecretadmin123/', admin.site.urls),
+    path('', include('app.urls')),
     path('shop/', include('shop.urls')),
     path('accounts/', include('accounts.urls')), 
      path('api/', include('api.urls')),
